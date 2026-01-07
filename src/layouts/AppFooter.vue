449219d07cd5efcac9c13e3ac461b70744102fe6
NetGeoOWild <goshan397@gmail.com>
2026-01-07 03:14:00 +0300

    update:shrek working

--- src/layouts/AppFooter.vue
+++ src/layouts/AppFooter.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { usePaginationStore } from '@/stores/paginationStore'
 const paginationStore = usePaginationStore()
+const isShowShrek = ref(false)
 </script>
 
 <template>
-  <footer class="footer mb-[250px] max-[470px]:mb-0">
+  <footer @click="isShowShrek = !isShowShrek" :class="{ shrek: isShowShrek }"
+    class="footer mb-[250px] max-[470px]:mb-0">
     <div class="container">
-      <div
-        class="bg-gray-500 px-15 py-5 max-[1220px]:px-5 hover:bg-transparent transition-all duration-700"
-      >
+      <div :class="{ 'bg-transparent': isShowShrek }"
+        class="bg-gray-500 px-15 py-5 max-[1220px]:px-5 hover:bg-transparent transition-all duration-700">
         <div class="flex items-center align-middle max-[768px]:flex-col-reverse">
           <button @click="paginationStore.goHome" class="block cursor-pointer">
-            <img class="block w-full max-w-[200px] max-[951px]:max-w-[150px] max-[591px]:max-w-[100px]" src="../assets/images/logo.png" alt="logo" />
+            <img class="block w-full max-w-[200px] max-[951px]:max-w-[150px] max-[591px]:max-w-[100px]"
+              src="../assets/images/logo.png" alt="logo" />
           </button>
           <p class="text-white text-lg max-[768px]:text-center">
             "GOGA ANIME OT BOGA" - сайт онлайн anime в хорошем качестве бесплатно.
-            <span class="block"
-              >P.S - Не благодари брат. (Ставь лайк, если тоже любишь Шрека)
+            <span class="block">P.S - Не благодари брат. (Ставь лайк, если тоже любишь Шрека)
             </span>
           </p>
         </div>
